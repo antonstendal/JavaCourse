2a78f029ca124373681f3c4e839f9214fa973f0f
Anton Sheiko <antonstendal19@gmail.com>
2024-10-18 16:10:20 +0200

    add lesson ComprassionOperators

---
+++ src/ComparisonOperators.java
@@ -0,0 +1,15 @@
+public class ComparisonOperators {
+    public static void main(String[] args) {
+        int firstNumber = 4;
+        int secondNumber = 6;
+
+
+
+        System.out.println(firstNumber > secondNumber);
+        System.out.println(firstNumber < secondNumber);
+        System.out.println(firstNumber >= secondNumber);
+        System.out.println(firstNumber <= secondNumber);
+        System.out.println(firstNumber == secondNumber);
+        System.out.println(firstNumber != secondNumber);
+    }
+}
