6b8f53ad0639422028b4452356b3a1b83aa2e2bf
formarkbeat@gmail.com <formarkbeat@gmail.com>
2024-10-19 15:44:54 +0200

    Add lesson Array

--- src/Arrays.java
+++ src/Arrays.java
@@ -5,16 +5,16 @@ public class Arrays {
         winterMonths[1] = "January";
         winterMonths[2] = "February";
 
-//        System.out.println(winterMonths[0] + winterMonths[1] + winterMonths[2]);
-        int[] lottoNumbers = new int[]{1,2,3,4,5,6};
-        /*System.out.println(lottoNumbers[0]);
+        System.out.println(winterMonths[0] + winterMonths[1] + winterMonths[2]);
+        int[] lottoNumbers = new int[]{1, 2, 3, 4, 5, 6};
+        System.out.println(lottoNumbers[0]);
         System.out.println(lottoNumbers[1]);
         System.out.println(lottoNumbers[2]);
         System.out.println(lottoNumbers[3]);
         System.out.println(lottoNumbers[4]);
         System.out.println(lottoNumbers[5]);
 
-        System.out.println(lottoNumbers.length);*/
+        System.out.println(lottoNumbers.length);
 
         for (int i = 0; i < winterMonths.length; i++) {
             System.out.println(winterMonths[i]);
