5111486a23d41189ade729c0d7ecf66df91c7ecc
Anton Sheiko <antonstendal19@gmail.com>
2024-10-20 10:50:22 +0200

    Add HomeWorkArrays

--- src/HomeWorkArrays.java
+++ src/HomeWorkArrays.java
@@ -1,15 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeWorkArrays {
     public static void main(String[] args) {
-        int num = 100;
-        for (int i = 0; i <= num; i++) {
-            if (i % 3 == 1) {
-                System.out.println(i);
-            }
-        }
+//        int num = 100;
+//        for (int i = 0; i <= num; i++) {
+//            if (i % 3 == 0) {
+//                System.out.println(i);
+//            }
+//        }
 
-        int[] numbers = {1,3,5};
-        System.out.println(numbers[2]);
-        System.out.println(numbers[1]);
-        System.out.println(numbers[0]);
+        int[] num = new int[]{1, 3, 2, 7, 5};
+        for (int i = 0; i < num.length/2    ; i++) {
+            int temp = num[i]; // for i=0 --> 1
+            num[i] = num[num.length - 1 - i]; // changed first element array on last element
+            num[num.length - 1 - i] = temp;
+        }
+        for (int i = 0; i < num.length; i++) {
+            System.out.println(num[i]);
+        }
     }
 }
