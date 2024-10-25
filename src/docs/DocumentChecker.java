e18311a64c902242f6aa569c8a6e7b683aeea33c
Anton Sheiko <antonstendal19@gmail.com>
2024-10-25 09:08:24 +0200

    Learn Polymorphism Changed class ChromeDriver FirefoxDriver WebDriversTest

---
+++ src/docs/DocumentChecker.java
@@ -0,0 +1,12 @@
+package docs;
+
+
+public class DocumentChecker {
+
+    public static void main(String[] args) {
+
+        Document doc = new ExcelDocument();
+        doc.getDescription();
+
+    }
+}
--- src/drivers/WebDriversTest.java
+++ src/drivers/WebDriversTest.java
@@ -2,11 +2,19 @@ package drivers;
 
 public class WebDriversTest {
     public static void main(String[] args) {
-        ChromeDriver chromeDriver = new ChromeDriver();
-        FirefoxDriver firefoxDriver = new FirefoxDriver();
-        chromeDriver.get();
-        chromeDriver.findGetElementBy();
-        firefoxDriver.get();
-        firefoxDriver.findGetElementBy();
+
+        WebDriver driver = getDriver("fireFox");
+        driver.get();
+        driver.findGetElementBy();
+
+    }
+
+    private static WebDriver getDriver(String name) {
+        if (name.equals("chrome")){
+            return new ChromeDriver();
+        } else if (name.equals("fireFox")) {
+            return new FirefoxDriver();
+        }
+        return null;
     }
 }
