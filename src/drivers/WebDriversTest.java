67f562db1ba1a5dacd48811392421cd6a2f156e4
Anton Sheiko <antonstendal19@gmail.com>
2024-10-28 06:54:27 +0100

    Learn ENUM Add new class ENUM DriverType and add changes to Class WebDriversTest

--- src/drivers/WebDriversTest.java
+++ src/drivers/WebDriversTest.java
@@ -1,20 +1,31 @@
 package drivers;
 
-public class  WebDriversTest {
+public class WebDriversTest {
     public static void main(String[] args) throws NoValidBrowserName {
 
-        WebDriver driver = getDriver("firFox");
+        DriverType[] driverTypes = DriverType.values();
+        for (int i = 0; i < driverTypes.length; i++){
+            System.out.println(driverTypes[i]);
+            System.out.println(driverTypes[i].name);
+            System.out.println(driverTypes[i].path);
+        }
+
+        DriverType chrome = DriverType.valueOf("CHROME");
+        System.out.println(chrome);
+        System.out.println(Enum.valueOf(DriverType.class,"FIREFOX"));
+
+        WebDriver driver = getDriver(DriverType.CHROME);
         driver.get();
         driver.findGetElementBy();
 
     }
 
-    private static WebDriver getDriver(String name) throws NoValidBrowserName {
-        if (name.equals("chrome")){
+    private static WebDriver getDriver(DriverType type) {
+        if (type.name.equals("chrome")) {
+            System.out.println(type.path);
             return new ChromeDriver();
-        } else if (name.equals("fireFox")) {
-            return new FirefoxDriver();
         }
-        throw new NoValidBrowserName("No valid browser name.");
+        System.out.println(type.path);
+        return new FirefoxDriver();
     }
 }
